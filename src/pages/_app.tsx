f89bab22b1db8fe50df4c535668141d91158a3cb
Subhan Mahmood <sm01800@surrey.ac.uk>
2023-04-30 19:13:09 +0100

    chore: format

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
-import { GlobalStyle } from '@/assets/styles/app';
-import '@/assets/styles/globals.css';
 import type { AppProps } from 'next/app';
+
+import { GlobalStyle } from '@/assets/styles/app';
+
+import '@/assets/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
